d8b70ad40eaf9667c0b88a6deefb5cf8055ef53c
Nazrath <nazrath.nawaz@yahoo.de>
2018-09-03 20:12:38 +0100

    first section inital commits

---
+++ retrominer_output/create_results_table.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#############################################################
+####     CREATE RESULTS TABLE FOR RETROMINER'S OUTPUT    ####
+#############################################################
+
+#                                                           #
+#               convert cpsx to txt files                   #
+#                filter for RT proteins                     #
+#           create super table with all results             #
+#                                                           #
+
+
+
+#============================================================#
+# sh create_results_table.sh ARGS
+# sh create_results_table.sh 
+#============================================================#
+
+DIR=/data/SBCS-BessantLab/naz/RetroMiner_to_RTPEA/retrominer_output
+
+#------------------------------------------------------------#
+#         Downloads spectral data for Project PXD            #
+#------------------------------------------------------------#
+
+# PXD=$1
+
+#------------------------------------------------------------#
+
+
+
+
+
+
